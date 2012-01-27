ceb812caa99d047b675861c0d54f123e7a052415
Evan Stoner <evan.stoner@gmail.com>
2012-01-26 17:40:21 -0800

    Put overall flow in loop

--- ci-server.rb
+++ ci-server.rb
@@ -1,3 +1,19 @@
+require_relative "./derpsy"
+require_relative "./config"
+
+# make this take parameters for login/pw
+client = Derpsy.client
+repo = Derpsy.config[:repo]
+
 loop do
-  # stuff
+  Derpsy::Retrieve.pull_requests(client, repo)
+  Derpsy::Repo.setup
+  Derpsy::Test.setup
+  Derpsy::Test.run
+  Derpsy::Test.interpret
+  Derpsy::Test.cleanup
+  Derpsy::Notify.build_message
+  Derpsy::Notify.comment
+  Derpsy::Notify.campfire
+  sleep 15
 end
