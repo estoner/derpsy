07402fc59246b1bec58e85ff9fdc30c483fe52d4
Evan Stoner <evan.stoner@gmail.com>
2012-01-26 17:30:09 -0800

    better skeleton structure, still need tests

--- derpsy.rb
+++ derpsy.rb
@@ -4,53 +4,75 @@ require_relative "./config.rb"
 
 module Derpsy
   
-  client = Octokit::Client.new :login => Derpsy.config[:login], :password => Derpsy.config[:password] 
+  def self.client
+    Octokit::Client.new :login => Derpsy.config[:login], :password => Derpsy.config[:password] 
+  end
 
   module Retrieve
-    def self.pull_requests(client)
-
-      client.pull_requests(Derpsy.config[:repo]).each do |req|
-        discussion = client.pull(Derpsy.config[:repo], req.number).discussion
-        ap discussion.last
-
-        if discussion.last.type == "Commit"
-          ap discussion.last 
-        end
+    def self.pull_requests(client, repo)
+      ap "retrieve the pull requests"
+      pulls = client.pull_requests repo
+      pulls.each do |pull|
+        pull.discussion = client.pull(repo, pull.number).discussion
       end
+      pulls.delete_if { |pull| pull.discussion.last.type != "Commit" }
     end
   end
 
-  # this should probably really just hold the pull req data...Retrieve should get all pulls where last.type == "Commit"
-  module Pulls
-    def self.something(pulls, client)
-
-      pulls.each do |req|
-        discussion = client.pull(Derpsy.config[:repo], req.number).discussion
-        ap discussion.last
-
-        if discussion.last.type == "Commit"
-          ap discussion.last 
-        end
-      end
-
+  module Repo
+    def self.setup
+      ap "set up repo"
+      # git checkout [integration-branch]
+      # git pull upstream [integration-branch]
+      # git checkout -b derp
+      # git fetch [requestor]
+      # git merge [requestor] [integration-branch] **** ideally this would be specific commits in request ****
     end
   end
-          # git checkout [integration-branch]
-          # git pull upstream [integration-branch]
-          # git checkout -b derp
-          # git fetch [requestor]
-          # git merge [requestor] [integration-branch] **** ideally this would be specific commits in request ****
-          # bundle install
-          # reruns.each do { [test cmd with no weird formatting] if pass then return "passed" }
-          # if fail then return "failed"
-          # update comments & Campfire:
-          #   if failed, close request
-          #   if no changes in features/*.feature update comments to complain no new tests
-          #   in comment, mention time it took to run tests, # of reruns and which failed
-          #   if more than 2 reruns, get snarky
-          #   if hardcore, run simplecov and complain if not 100% coverage
-          # git checkout master
-          # git branch -D derp
+
+  module Test
+    def self.setup
+      ap "bundle install"
+      # bundle install
+    end
+
+    def self.run
+      ap "run the tests"
+      # reruns.each do { [test cmd with no weird formatting] if pass then return "passed" }
+      # if fail then return "failed"
+    end
+
+    def self.interpret
+      ap "interpret the results"
+      # figure out what the F the test results mean
+    end
+
+    def self.cleanup
+      ap "clean up the repo"
+      # git checkout master
+      # git branch -D derp
+    end
+
+  end
+
+  module Notify
+
+    def self.build_message
+      ap "build the message"
+      #   if no changes in features/*.feature update comments to complain no new tests
+      #   in comment, mention time it took to run tests, # of reruns and which failed
+      #   if more than 2 reruns, get snarky
+      #   if hardcore, run simplecov and complain if not 100% coverage
+    end
+
+    def self.comment
+      ap "post comment to GitHub"
+      #   if failed, close request
+    end
+
+    def self.campfire
+      ap "send notification to Campfire"
+    end
   #
   # sleep 15
   # handle errors throughout
