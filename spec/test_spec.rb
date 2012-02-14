ad954d8bd551c2e9531accdd3b0bb986b7ca7486
Evan Stoner <evan.stoner@gmail.com>
2012-02-13 17:28:53 -0800

    Test.setup specs pretty decent

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -6,16 +6,28 @@ require 'derpsy/test'
 describe Derpsy::Test do
 
   before do
-    @pull = #marshalled pull
-
-    it "must set up a local git repo when given a pull request" do
-      @repo = Derpsy::Test.setup(@pull)
-    end
-
+    pull = #marshalled pull
+    @repo = Derpsy::Test.setup(pull)
+    Dir.chdir(@repo.directory)
   end
 
-  it "must have the expected hash at HEAD" do
-    1.must_equal 1
+  describe "when it sets up the test repo" do
+
+    it "must be a valid repo" do
+      msg = IO.popen("git rev-parse --git-dir").readlines.first
+      msg.must_equal ".git\n"
+    end
+
+    it "must set the expected hash to HEAD" do
+      hash = IO.popen("git show-ref HEAD").readlines[0].split(" ").first
+      hash.must_equal "89adf24e940ac8b22da6f0da52e57d55bc5b7848"
+    end
+
+    it "must have the correct bundle installed" do
+      msg = IO.popen("bundle check").readlines.first
+      msg.must_equal "The Gemfile's dependencies are satisfied\n"
+    end
+
   end
 
 end
