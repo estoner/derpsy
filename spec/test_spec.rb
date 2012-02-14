89adf24e940ac8b22da6f0da52e57d55bc5b7848
Evan Stoner <evan.stoner@gmail.com>
2012-02-13 16:59:51 -0800

    test twiddling

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,9 +1,21 @@
 require 'minitest/autorun'
-require 'minitest/emoji'
+require 'minitest/pride'
+require 'minitest/mock'
 require 'derpsy/test'
 
 describe Derpsy::Test do
-  it "must derp" do
+
+  before do
+    @pull = #marshalled pull
+
+    it "must set up a local git repo when given a pull request" do
+      @repo = Derpsy::Test.setup(@pull)
+    end
+
+  end
+
+  it "must have the expected hash at HEAD" do
     1.must_equal 1
   end
+
 end
