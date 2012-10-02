b4010ebf63b888e34f1223238399993be4311151
Evan Stoner <evan.stoner@gmail.com>
2012-10-02 11:08:03 -0700

    Added daemon options to help debug crashes.

--- lib/derpsyd.rb
+++ lib/derpsyd.rb
@@ -1,4 +1,14 @@
 require 'rubygems'
 require 'daemons'
+require File.expand_path('../../config', __FILE__)
 
-Daemons.run( File.join(File.dirname(__FILE__), 'derpsy-runner.rb') )
+config = Derpsy.config
+
+daemon_options = { :log_dir => config[:working_directory],
+            :dir_mode => :normal,
+            :dir => config[:working_directory],
+            :backtrace => true,
+            :monitor => true
+          }
+
+Daemons.run( File.join(File.dirname(__FILE__), 'derpsy-runner.rb'), daemon_options )
